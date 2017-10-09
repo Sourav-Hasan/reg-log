38c3ffffe7ec7f87594e00ec3c98852d09d7287f
Sourav-Hasan <sourav.kurigram@gmail.com>
2017-10-09 23:10:55 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include_once 'class.user.php';
+$user = new User();
+ $uid = $_SESSION['uid'];
+if (!$user->get_session()){
+ header("location:login.php");
+}
+if (isset($_GET['q'])){
+ $user->user_logout();
+ header("location:login.php");
+ }
+ ?>
+<div id="container">
+<div id="header"><a href="home.php?q=logout">LOGOUT</a></div>
+<div id="main-body">
+<h1>Hello <?php $user->get_fullname($uid); ?></h1>
+</div>
+<div id="footer"></div>
+</div>
---
+++ users.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 09, 2017 at 07:01 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `uname` varchar(30) DEFAULT NULL,
+  `upass` varchar(50) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `uemail` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `uname`, `upass`, `fullname`, `uemail`) VALUES
+(1, 'sourav', '123', 'rezwan sourav', 's@gmail.com'),
+(2, 'm2', '900150983cd24fb0d6963f7d28e17f72', 'Mishu Khan', 'm2@gmail.com'),
+(3, 'ab', 'c4ca4238a0b923820dcc509a6f75849b', 'ab', 'ab@gmail.com'),
+(5, 'saifur', '202cb962ac59075b964b07152d234b70', 'saifur rahman', 'saifur@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `uname` (`uname`),
+  ADD UNIQUE KEY `uemail` (`uemail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
